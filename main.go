2aff875e1ca5893736cd8de785eec1897d25dc24
nbeisenb <nbeusenbek@mail.ru>
2024-09-01 13:45:10 +0500

    getfield

--- main.go
+++ main.go
@@ -5,21 +5,28 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 type CSVParser interface {
 	ReadLine(r io.Reader) (*string, error)
+	GetField(n int) (string, error)
 }
 
-var ErrQuote = errors.New("excess or missing \" in quoted-field") // ErrFieldCount = errors.New("wrong number of fields")
+var (
+	ErrQuote      = errors.New("excess or missing \" in quoted-field")
+	ErrFieldCount = errors.New("wrong number of fields")
+)
 
-type CSV struct{}
+type CSV struct {
+	line string
+}
 
-func (c CSV) ReadLine(r io.Reader) (*string, error) {
+func (c *CSV) ReadLine(r io.Reader) (*string, error) {
 	buff := make([]byte, 1)
-	line := ""
 	lastchar := ""
 	inQuotes := false
+	c.line = ""
 
 	for {
 		_, err := r.Read(buff)
@@ -28,7 +35,7 @@ func (c CSV) ReadLine(r io.Reader) (*string, error) {
 			if inQuotes {
 				return nil, ErrQuote
 			}
-			return &line, err
+			return &c.line, err
 		}
 
 		if char == `"` {
@@ -40,9 +47,48 @@ func (c CSV) ReadLine(r io.Reader) (*string, error) {
 			inQuotes = !inQuotes
 		}
 
-		line += char
+		c.line += char
 		lastchar = char
 	}
+}
+
+func (c *CSV) GetField(n int) (string, error) {
+	if n < 0 || n > (1+strings.Count(c.line, ",")) {
+		return "", ErrFieldCount
+	}
+
+	commaN := 0
+	lastComma := -1
+
+	for i := 0; i < len(c.line); i++ {
+		if string(c.line[i]) == "," {
+			commaN += 1
+			if commaN == n {
+				s := string(c.line[lastComma+1 : i])
+				if len(s) > 0 && s[0] == '"' {
+					s = s[1:]
+				}
+				if len(s) > 0 && s[len(s)-1] == '"' {
+					s = s[:len(s)-1]
+				}
+				return s, nil
+			}
+			lastComma = i
+		}
+	}
+
+	s := string(c.line[lastComma+1:])
+	if len(s) > 0 && s[0] == '"' {
+		s = s[1:]
+	}
+	if len(s) > 0 && s[len(s)-1] == '"' {
+		s = s[:len(s)-1]
+	}
+	return s, nil
+}
+
+func NewCsv() *CSV {
+	return new(CSV)
 }
 
 func main() {
@@ -53,18 +99,21 @@ func main() {
 	}
 	defer file.Close()
 
-	var csvparser CSVParser = CSV{}
+	CSVParser := NewCsv()
 
 	for {
-		line, err := csvparser.ReadLine(file)
+		_, err := CSVParser.ReadLine(file)
 		if err != nil {
 			if err == io.EOF {
-				fmt.Println(*line)
+				field, _ := CSVParser.GetField(3)
+				fmt.Println(field)
 				break
 			}
 			fmt.Println("Error reading line:", err)
 			return
 		}
-		fmt.Println(*line)
+
+		field, _ := CSVParser.GetField(3)
+		fmt.Println(field)
 	}
 }
